737a969a044302868a32181c16cb5f5116d359f6
Min La Wee Chan <u6520189@au.edu>
2025-09-18 19:55:19 +0700

    Restore authentication files from backup

---
+++ src/components/auth/AdminRoute.js
@@ -0,0 +1,11 @@
+'use client';
+
+import ProtectedRoute from './ProtectedRoute';
+
+export default function AdminRoute({ children }) {
+  return (
+    <ProtectedRoute requireAdmin={true}>
+      {children}
+    </ProtectedRoute>
+  );
+} 
